0ddfc63d246af4ef71306ccf4a0adc141f079715
yongx <47638800+botakist@users.noreply.github.com>
2024-01-20 23:10:36 +0800

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+## install dependencies
+sudo apt update
+sudo apt install apache2 \
+                 ghostscript \
+                 libapache2-mod-php \
+                 mysql-server \
+                 php \
+                 php-bcmath \
+                 php-curl \
+                 php-imagick \
+                 php-intl \
+                 php-json \
+                 php-mbstring \
+                 php-mysql \
+                 php-xml \
+                 php-zip -y
+## install wordpress
+sudo mkdir -p /srv/www
+sudo chown www-data: /srv/www
+curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
+sudo tee -a /etc/apache2/sites-available/wordpress.conf <<EOF
+<VirtualHost *:80>
+    DocumentRoot /srv/www/wordpress
+    <Directory /srv/www/wordpress>
+        Options FollowSymLinks
+        AllowOverride Limit Options FileInfo
+        DirectoryIndex index.php
+        Require all granted
+    </Directory>
+    <Directory /srv/www/wordpress/wp-content>
+        Options FollowSymLinks
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+sudo systemctl reload apache2
+sudo a2ensite wordpress
+sudo a2dissite 000-default
+sudo service apache2 reload
+
+# setup mysql database
+sudo tee -a /home/ubuntu/db_setup.sql <<EOF
+CREATE DATABASE wordpress;
+CREATE USER wordpress@localhost IDENTIFIED BY 'Welcome123';
+GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO wordpress@localhost;
+FLUSH PRIVILEGES;
+EOF
+
+sudo mysql -u root < /home/ubuntu/db_setup.sql
+# start service
+sudo service mysql start
+# 6. Configure WordPress to connect to the database
+sudo -u www-data cp /srv/www/wordpress/wp-config-sample.php /srv/www/wordpress/wp-config.php
+sudo -u www-data sed -i 's/database_name_here/wordpress/' /srv/www/wordpress/wp-config.php
+sudo -u www-data sed -i 's/username_here/wordpress/' /srv/www/wordpress/wp-config.php
+sudo -u www-data sed -i 's/password_here/Welcome123/' /srv/www/wordpress/wp-config.php
+
+SALT=$(curl -L https://api.wordpress.org/secret-key/1.1/salt/)
+STRING='put your unique phrase here'
+printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s /srv/www/wordpress/wp-config.php
+
