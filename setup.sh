1603cf56b83256539ed3aa14a438460117af52ea
yongx <47638800+botakist@users.noreply.github.com>
2024-01-22 17:01:03 +0800

    Update setup.sh: add comments

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-## install dependencies
+# install dependencies
 sudo apt update
 sudo apt install apache2 \
                  ghostscript \
@@ -15,10 +15,11 @@ sudo apt install apache2 \
                  php-mysql \
                  php-xml \
                  php-zip -y
-## install wordpress
+# install wordpress
 sudo mkdir -p /srv/www
 sudo chown www-data: /srv/www
 curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
+# setup apache web server to host wordpress
 sudo tee -a /etc/apache2/sites-available/wordpress.conf <<EOF
 <VirtualHost *:80>
     DocumentRoot /srv/www/wordpress
@@ -47,15 +48,16 @@ GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO wordpress@
 FLUSH PRIVILEGES;
 EOF
 
-sudo mysql -u root < /home/ubuntu/db_setup.sql
+sudo mysql -u root < /home/ubuntu/db_setup.sql # runs the above wordpress setup sql statements on mysql
 # start service
 sudo service mysql start
-# 6. Configure WordPress to connect to the database
+# configure WordPress to connect to the database
 sudo -u www-data cp /srv/www/wordpress/wp-config-sample.php /srv/www/wordpress/wp-config.php
 sudo -u www-data sed -i 's/database_name_here/wordpress/' /srv/www/wordpress/wp-config.php
 sudo -u www-data sed -i 's/username_here/wordpress/' /srv/www/wordpress/wp-config.php
 sudo -u www-data sed -i 's/password_here/Welcome123/' /srv/www/wordpress/wp-config.php
 
+# make login secure
 SALT=$(curl -L https://api.wordpress.org/secret-key/1.1/salt/)
 STRING='put your unique phrase here'
 printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s /srv/www/wordpress/wp-config.php
